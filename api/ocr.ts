6d15dbdaf5e97c04b08aba2e5f26060a778c7d76
isthismyniche <manishnair999@gmail.com>
2025-10-20 23:01:17 +0800

    tweaked y axis tolerance to 11

--- api/ocr.ts
+++ api/ocr.ts
@@ -67,7 +67,7 @@ async function tryGoogleVision(imageData: string) {
 
   // Group words into lines based on Y coordinate (tolerance of 8 pixels)
   const lines: Word[][] = [];
-  const yTolerance = 8;
+  const yTolerance = 11;
 
   wordObjects.forEach(word => {
     let addedToLine = false;
